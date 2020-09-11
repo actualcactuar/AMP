48dae32c9b4afbbb1aa6bea1c37848523c3058bc
jiri <kiviniemi.jiri@gmail.com>
2020-09-11 13:46:25 +0300

    bassEQ

--- site/main.js
+++ site/main.js
@@ -1,5 +1,6 @@
 import { buildOptions, getAudio, drawVisualizer, crispCanvas } from './js/utils.js';
 
+// DOM
 const volume = document.getElementById('volume');
 const bass = document.getElementById('bass');
 const mid = document.getElementById('mid');
@@ -7,9 +8,29 @@ const treble = document.getElementById('treble');
 const visualizer = document.getElementById('visualizer');
 const input = document.getElementById('input');
 const output = document.getElementById('output');
+
+// AUDIO CONTEXT
 const fftSize = 512
 const context = new AudioContext();
 const analyzerNode = new AnalyserNode(context, { fftSize })
+const gainNode = new GainNode(context, { gain: volume.value })
+const bassEQ = new BiquadFilterNode(context, {
+    frequency: 500,
+    type: 'lowshelf',
+    gain: bass.value
+})
+
+function setEventListeners() {
+    window.onresize = crispCanvas;
+    volume.oninput = event => {
+        const value = parseFloat(event.target.value);
+        gainNode.gain.setTargetAtTime(value, context.currentTime, 0.1)
+    }
+    bass.oninput = event => {
+        const value = parseInt(event.target.value);
+        bassEQ.gain.setTargetAtTime(value, context.currentTime, 0.1)
+    }
+}
 
 async function getAudioIO() {
     const allDevices = await navigator.mediaDevices.enumerateDevices();
@@ -26,6 +47,8 @@ async function setupAudioContext() {
     }
     const source = context.createMediaStreamSource(audio);
     source
+        .connect(bassEQ)
+        .connect(gainNode)
         .connect(analyzerNode)
         .connect(context.destination)
 }
@@ -33,9 +56,9 @@ async function setupAudioContext() {
 async function App() {
     await getAudioIO();
     await setupAudioContext();
-    drawVisualizer(visualizer,analyzerNode);
+    drawVisualizer(visualizer, analyzerNode);
     crispCanvas(visualizer);
-    window.onresize = crispCanvas;
+    setEventListeners();
 }
 
 App();
